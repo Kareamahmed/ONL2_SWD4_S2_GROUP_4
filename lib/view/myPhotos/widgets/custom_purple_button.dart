4f00ce90e51cb4c26e9a6be54ca3b4e4fe5d1ba1
Mahmou-d6356567377 <ma2327424@gmial.com>
2025-04-13 19:05:28 +0200

    f1

--- lib/view/myPhotos/widgets/custom_purple_button.dart
+++ lib/view/myPhotos/widgets/custom_purple_button.dart
@@ -8,26 +8,31 @@ class CustomPurpleButton extends StatelessWidget {
   final void Function()? onpressed;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-    gradient: LinearGradient(
-      colors: [Color(0xFF7BA5FF), Color(0xFF9F87FF)], // Adjust gradient colors if you want
-      begin: Alignment.topLeft,
-      end: Alignment.bottomRight,
-    ),
-    borderRadius: BorderRadius.circular(30), // High value for pill shape
+    return GestureDetector(
+      onTap: onpressed,
+      child: Container(
+        decoration: BoxDecoration(
+      gradient: LinearGradient(
+        colors: [Color(0xFF7BA5FF), Color(0xFF9F87FF)], // Adjust gradient colors if you want
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
       ),
-      child: TextButton(
-    onPressed: onpressed,
-    style: TextButton.styleFrom(
-      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(30),
+      borderRadius: BorderRadius.circular(30), // High value for pill shape
+        ),
+        child: TextButton(
+      onPressed: () {
+        // Your action here
+      },
+      style: TextButton.styleFrom(
+        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+        foregroundColor: Colors.white, // Text color
+        textStyle: TextFonts.kwhitenormalfont12
       ),
-      foregroundColor: Colors.white, // Text color
-      textStyle: TextFonts.kwhitenormalfont12
-    ),
-    child: Text(text , style: TextFonts.kwhitenormalfont12,),
+      child: Text(text , style: TextFonts.kwhitenormalfont12,),
+        ),
       ),
     )
 ;
